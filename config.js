db06af77cfad63e4488035b05ff2db3cffd229c3
MohamadKerdi <72773482+MohamadKerdi@users.noreply.github.com>
2020-10-13 04:18:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "NzYyMDgxODUzNDk4MTk1OTg5.X3j-CA.o5ujdAM0lJWUvyJBZHFvLaXqiyA",
+  TOKEN: "NzY1MzgxNzkzMjIxNzA1NzY5.X4T_Vw.RvmSOJBlq0eRn232e5SwAky8wfU",
   YT_API_KEY: "",
   prefix: "!", //بريفيكس او الامر حقك
   devs: ["538105848669405234"] //ايدي حقك
